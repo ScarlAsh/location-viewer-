264c076d5b31209d64488708a51dce7896655929
Esraa Mohamed <124937321+ScarlAsh@users.noreply.github.com>
2023-05-11 02:56:17 +0300

    Update main.js

--- Getting location/main.js
+++ Getting location/main.js
@@ -1,17 +1,31 @@
 let lat;
 let long;
-function myfun(){
+
+navigator.geolocation.getCurrentPosition(showlocation);
+
+function showlocation(pos)
+	{
+		lat = pos.coords.latitude;
+	    long = pos.coords.longitude;
+	}
+
+
+function view(){
 	navigator.geolocation.getCurrentPosition(showlocation);
+	function showlocation(pos){
+	
+		alert("the latitude of your location is \n" + pos.coords.latitude + "\n"+ "the longitude of your location is \n" +pos.coords.longitude);
+	
+	}
 
 }
-function showlocation(pos){
-	lat=pos.coords.latitude;
-	long=pos.coords.longitude;
-	alert("the latitude of your location is \n" + pos.coords.latitude + "\n"+ "the longitude of your location is \n" +pos.coords.longitude);
 
-}
 
-function myfunction(){
+function viewOnMap(){
+	
 	window.open("http://maps.google.com/maps?q=" + lat + ",+" + long ,'_blank');
 }
+
+
+
 
